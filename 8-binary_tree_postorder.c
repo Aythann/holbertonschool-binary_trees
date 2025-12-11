e985de76654f059b26e9058f28deee5178ffdc15
Aythann <aythancristovao@gmail.com>
2025-12-11 11:02:04 +0100

    fix: Betty style compliance across all source files

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,12 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - that goes through a binary tree using post-order traversal
- * @tree: pointer to the root node of the tree to traverse
- * @func: pointer to a function to call for each node. 
- * 		  The value in the node must be passed as a parameter to this function.
- * 
- * return : nothing
+ * binary_tree_postorder - Goes through a binary tree in postorder traversal
+ * @tree: Pointer to the root node
+ * @func: Pointer to a function called for each node
+ *
+ * Return: Nothing
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
