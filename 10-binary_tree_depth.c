fa8cc8cc7e91033c5ecc671fecb961858fc89d61
Aythann <aythancristovao@gmail.com>
2025-12-11 11:02:16 +0100

    fix: Betty style compliance across all source files

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - measures the depth of a node in a binary tree
- * @tree: pointer to the node to measure the depth.
- * 
- * return : Depth of the node, or 0 if tree is NULL.
+ * binary_tree_depth - Measures the depth of a node in a binary tree
+ * @tree: Pointer to the node to measure the depth
+ *
+ * Return: Depth of the node, or 0 if tree is NULL
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
