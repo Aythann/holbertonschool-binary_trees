5daf909b19cdc77e5475f16a9b9033f36d40fa45
Aythann <aythancristovao@gmail.com>
2025-12-11 11:01:54 +0100

    fix: Betty style compliance across all source files

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,12 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - that goes through a binary tree using pre-order traversal
- * @tree: pointer to the root node of the tree to traverse
- * @func: pointer to a function to call for each node. 
- * 		  The value in the node must be passed as a parameter to this function.
- * 
- * return : nothing
+ * binary_tree_preorder - Goes through a binary tree in preorder traversal
+ * @tree: Pointer to the root node
+ * @func: Pointer to a function called for each node
+ *
+ * Return: Nothing
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
