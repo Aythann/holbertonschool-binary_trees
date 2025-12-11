f859cdb2fa08a2022e93a35775ef2ad98c4c8229
Aythann <aythancristovao@gmail.com>
2025-12-11 11:02:43 +0100

    fix: Betty style compliance across all source files

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * check_depth - finds the depth of leaves
- * @tree: pointer to the root node of the tree to check
+ * check_depth - Computes the depth of the leftmost leaf
+ * @tree: Pointer to the tree
  *
- * Return: depth of tree
+ * Return: Depth of the leftmost leaf
  */
 static int check_depth(const binary_tree_t *tree)
 {
@@ -20,34 +20,34 @@ static int check_depth(const binary_tree_t *tree)
 }
 
 /**
- * check_perfect - check if the tree is perfect recursively
- * @tree: pointer to the current node
- * @depth: depth of leaves
- * @level: level in tree
+ * check_perfect - Checks recursively if a tree is perfect
+ * @tree: Pointer to the current node
+ * @depth: Depth of the leaves
+ * @level: Current level in the tree
  *
- * Return: 1 if perfect and 0 if NULL
+ * Return: 1 if perfect, 0 otherwise
  */
 static int check_perfect(const binary_tree_t *tree, int depth, int level)
 {
 	if (tree == NULL)
 		return (1);
 
-	if (tree->left == NULL && tree->right == NULL)	
+	if (tree->left == NULL && tree->right == NULL)
 		return (depth == level);
 
 	if (tree->left == NULL || tree->right == NULL)
 		return (0);
 
-	return (check_perfect(tree->left, depth, level + 1) && check_perfect(tree->right, depth, level + 1));	
+	return (check_perfect(tree->left, depth, level + 1)
+			&& check_perfect(tree->right, depth, level + 1));
 }
 
 /**
- * binary_tree_is_perfect - checks if a binary tree is perfect
- * @tree: pointer to the root node of the tree to check
+ * binary_tree_is_perfect - Checks if a binary tree is perfect
+ * @tree: Pointer to the root node
  *
- * Return: 
+ * Return: 1 if perfect, 0 otherwise
  */
-
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	int depth;
