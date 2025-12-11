bcd59ab878b2fd5352dca51b67d54e8d52ac2228
Aythann <aythancristovao@gmail.com>
2025-12-11 11:01:24 +0100

    fix: Betty style compliance across all source files

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - inserts a node as the left-child of another node
- * @parent: pointer to the node to insert the left-child in
- * @value: the value to store in the new node
- * 
- * return : binary tree new node
+ * binary_tree_insert_left - Inserts a node as the left child of a node
+ * @parent: Pointer to the parent node
+ * @value: Value stored in the new node
+ *
+ * Return: Pointer to the created node, or NULL on failure
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
@@ -17,7 +17,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
 		return (NULL);
-		
+
 	new_node->n = value;
 	new_node->parent = parent;
 	new_node->left = parent->left;
