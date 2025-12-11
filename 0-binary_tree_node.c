e9a178c1e0f3c2c860b1c97834ff1cc6c3431506
Aythann <aythancristovao@gmail.com>
2025-12-11 11:01:15 +0100

    fix: Betty style compliance across all source files

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - creates a binary tree node
- * @parent: pointer to the parent node of the node to create
- * @value: the value to put in the new node
- * 
- * return : binary tree node
+ * binary_tree_node - Creates a new binary tree node
+ * @parent: Pointer to the parent node
+ * @value: Value stored in the new node
+ *
+ * Return: Pointer to the new node, or NULL on failure
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
@@ -19,6 +19,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	node->parent = parent;
 	node->left = NULL;
 	node->right = NULL;
-	
+
 	return (node);
 }
