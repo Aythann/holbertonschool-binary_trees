e0d17683a5e1c605e04adbc81b5861f61e72e716
Aythann <aythancristovao@gmail.com>
2025-12-11 11:01:59 +0100

    fix: Betty style compliance across all source files

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,12 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder - that goes through a binary tree using in-order traversal
- * @tree: pointer to the root node of the tree to traverse
- * @func: pointer to a function to call for each node. 
- * 		  The value in the node must be passed as a parameter to this function.
- * 
- * return : nothing
+ * binary_tree_inorder - Goes through a binary tree in inorder traversal
+ * @tree: Pointer to the root node
+ * @func: Pointer to a function called for each node
+ *
+ * Return: Nothing
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
